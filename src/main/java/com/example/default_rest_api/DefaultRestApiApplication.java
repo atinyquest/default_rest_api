b21eab9d08819b479c06f15db4f60486aaa73d54
dahye.kim <kdh012097@blackholic.com>
2025-01-07 15:02:24 +0900

    초창기 세팅

---
+++ src/main/java/com/example/default_rest_api/DefaultRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.default_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/default_rest_api/DefaultRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.default_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
